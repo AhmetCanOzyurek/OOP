0d7835599f66b4944ee0af05080b508702519c08
AhmetCan <a.canozyurek@gmail.com>
2023-08-09 01:42:17 +0300

    My first OOP

---
+++ EmptyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Ogrenci ogrenci = new Ogrenci();
+        ogrenci.adi = "Ahmet";
+        ogrenci.numarasi = 1234;
+
+        Ogrenci ogrenci1 = new Ogrenci();
+        ogrenci1.adi = "Mehmet";
+        ogrenci1.numarasi = 123123;
+
+
+        ogrenci.OgrenciBilgisiYazdir();
+        ogrenci1.OgrenciBilgisiYazdir();
+
+        Console.ReadLine();
+    }
+}
+
+public class Ogrenci
+{
+    public string adi;
+    public int numarasi;
+
+    public void OgrenciBilgisiYazdir()
+    {
+        Console.WriteLine("Ogrenci adi " + adi);
+        Console.WriteLine("Ogrenci numarasi" + numarasi);
+    }
+}
